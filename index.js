b167bc81f7e1cd91e58f1077925e05532a6df6a8
scf89 <scarreraf@gmail.com>
2024-11-12 22:24:13 +0100

    update

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ const eventuserRoutes = require('./src/api/routes/eventuserRoutes');
 
 const app = express();
 
-const corsOptions = {
-  origin: ['http://localhost:5173'],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+const allowedOrigins = ['http://localhost:5173'];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitir solicitudes sin origen (como herramientas de prueba)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
